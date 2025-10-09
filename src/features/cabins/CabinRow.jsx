d11041dc195726ad7fb480df85be1ff521e82caa
Ujjal Shrestha <ujjalshrestha3141592@gmail.com>
2025-10-09 18:57:03 +0545

    Add react-hot-toast and use them in cabins.jsx for showing how deletion went

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -60,9 +61,9 @@ function CabinRow({ cabin }) {
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
-      alert("Deleted succesfully");
+      toast.success("Deleted succesfully");
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (
--- src/ui/Row.jsx
+++ src/ui/Row.jsx
@@ -3,19 +3,16 @@ import styled, { css } from "styled-components";
 const Row = styled.div`
   display: flex;
 
-  ${({ type = "horizontal" }) =>
-    type === "horizontal" &&
-    css`
-      justify-content: space-between;
-      align-items: center;
-    `}
-
-  ${(props) =>
-    props.type === "vertical" &&
-    css`
-      flex-direction: column;
-      gap: 1.6rem;
-    `}
+  ${({ type }) =>
+    type === "vertical"
+      ? css`
+          flex-direction: column;
+          gap: 1.6rem;
+        `
+      : css`
+          justify-content: space-between;
+          align-items: center;
+        `}
 `;
 
 export default Row;
